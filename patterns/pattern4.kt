237558996ee08d8248f54d0bdcdc3374a291f586
Balveer Dhanoriya <balveer2011@live.in>
2024-06-13 22:37:43 +0530

    pattern 4

---
+++ patterns/pattern4.kt
@@ -0,0 +1,21 @@
+/**
+ * 			Pattern 1
+ * 			1
+ * 			2 2
+ * 			3 3 3
+ * 			4 4 4 4
+ *          5 5 5 5 5
+ */
+
+fun main() {
+    printPattern(5)
+}
+
+fun printPattern(n: Int) {
+    for(i in 1..5) {
+        for(j in 1..i) {
+        	print("$i ")
+    	}
+        println()
+    }
+}
